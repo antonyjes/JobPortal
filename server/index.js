c6b99ecbc65bd60f1c1bf0b0bd1e6955a117685d
antonyjes <72809149@continental.edu.pe>
2024-02-24 19:40:56 -0500

    Moongose setup

--- server/index.js
+++ server/index.js
@@ -8,54 +8,54 @@ import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 app.use(express.json({ limit: "30mb" }));
-app.use(express.urlencoded())
+app.use(express.urlencoded());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "public/assets")))
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 mongoose.set("strictQuery", true);
 
 // FILE STORAGE
 const adminStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets/admins");
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + "-" + uuidv4();
-        cb(null, uniqueSuffix + "-" + file.originalname);
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "public/assets/admins");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + uuidv4();
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
 
 const userStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets/users");
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + "-" + uuidv4();
-        cb(null, uniqueSuffix + "-" + file.originalname);
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "public/assets/users");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + uuidv4();
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
 
 const recruiterStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets/recruiters");
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + "-" + uuidv4();
-        cb(null, uniqueSuffix + "-" + file.originalname);
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "public/assets/recruiters");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + uuidv4();
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
 
 const cvStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets/cvs");
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + "-" + uuidv4();
-        cb(null, uniqueSuffix + "-" + file.originalname);
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "public/assets/cvs");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + uuidv4();
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
 
 const adminUpload = multer({ storage: adminStorage });
 const userUpload = multer({ storage: userStorage });
@@ -63,3 +63,15 @@ const recruiterUpload = multer({ storage: recruiterStorage });
 const cvUpload = multer({ storage: cvStorage });
 
 //ROUTE WITH FILES
+
+//MOONGOSE SETUP
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server port: ${PORT}`));
+  })
+  .catch((error) => console.log(error));
