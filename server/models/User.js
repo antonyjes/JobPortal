3a770554a0005ed065b40cc9b431f17607c271fc
antonyjes <72809149@continental.edu.pe>
2024-02-24 23:25:33 -0500

    User model

---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: String,
+        lastName: String,
+        email: {type: String, unique: true},
+        password: String,
+        role: String,
+        picturePath: {type: String, default: ""},
+        roleJob: String,
+        address: String,
+        city: String,
+        cvPath: {type: String, default: ""},
+    },
+    {timestamps: true}
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
