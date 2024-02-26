b0c0140d1f583a47f14a5032807f23f515ff8940
antonyjes <72809149@continental.edu.pe>
2024-02-25 23:06:28 -0500

    Main config with state

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import storage from 'redux-persist/lib/storage'
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist'
+import authReducer from "./state"
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistConfig = {key: "root", storage, version: 1};
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PERSIST, PAUSE, PURGE, REGISTER]
+      }
+    })
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
