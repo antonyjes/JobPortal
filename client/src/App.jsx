329145cb21d5cbd82bb2f516b532a352421cb538
antonyjes <72809149@continental.edu.pe>
2024-02-26 22:24:47 -0500

    App routes and toast package

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer />
     </>
   )
 }
