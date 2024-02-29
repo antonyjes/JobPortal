f5532cce7f746fb80c6a10114a6ce1006f685a66
antonyjes <72809149@continental.edu.pe>
2024-02-29 13:20:01 -0500

    routes by user client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import Login from "./pages/auth/Login"
 import Register from "./pages/auth/Register"
+import { useSelector } from "react-redux"
+import AdminMain from "./pages/main/AdminMain"
+import UserMain from "./pages/main/UserMain"
+import RecruiterMain from "./pages/main/RecruiterMain"
 
 function App() {
+  const isAuth = Boolean(useSelector((state) => state.token));
+  const user = useSelector((state) => state.user);
+  const isAdmin = user && user.role === "Admin";
+  const isRecruiter = user && user.role === "Recruiter";
+  const isUser = user && user.role === "User";
+
   return (
     <>
       <BrowserRouter>
@@ -13,6 +23,9 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/admin/home" element={isAuth && isAdmin ? <AdminMain /> : <Navigate to="/" />} />
+          <Route path="/user/home" element={isAuth && isUser ? <UserMain /> : <Navigate to="/" />} />
+          <Route path="/recruiter/home" element={isAuth && isRecruiter ? <RecruiterMain /> : <Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
       <ToastContainer />
