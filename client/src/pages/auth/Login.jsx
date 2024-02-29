7356c0f5c08e94cbaad8ed8317a9f6b29f8a3fdc
antonyjes <72809149@continental.edu.pe>
2024-02-29 13:22:07 -0500

    fix link server login

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const loggedInResponse = await fetch("http://localhost:3003/login", {
+        const loggedInResponse = await fetch("http://localhost:3003/auth/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({email, password})
