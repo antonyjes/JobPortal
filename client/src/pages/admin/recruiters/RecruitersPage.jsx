9d04d768189088466bf299f33db7f38885933010
antonyjes <72809149@continental.edu.pe>
2024-03-01 15:36:56 -0500

    recruites page for admin

--- client/src/pages/admin/recruiters/RecruitersPage.jsx
+++ client/src/pages/admin/recruiters/RecruitersPage.jsx
@@ -1,11 +1,14 @@
 import { setRecruiters } from "@/state";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
+import LayoutAdmin from "../LayoutAdmin";
+import { RecruiterClient } from "./components/client";
 
 const RecruitersPage = () => {
     const dispatch = useDispatch();
     const recruiters = useSelector((state) => state.recruiters);
     const token = useSelector((state) => state.token);
+    const [showModal, setShowModal] = useState(false);
     
     const getRecruiters = async () => {
         const response = await fetch("http://localhost:3003/recruiters", {
@@ -23,7 +26,13 @@ const RecruitersPage = () => {
 
     return(
         <>
-            
+            <LayoutAdmin>
+                <div className="flex-col">
+                    <div className="flex-1 space-y-4 p-8 pt-6">
+                        <RecruiterClient data={recruiters} setShowModal={setShowModal} />
+                    </div>
+                </div>
+            </LayoutAdmin>
         </>
     )
 }
