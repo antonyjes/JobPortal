839d07cccfebdcef336063d6612badcdc3e3ca5b
antonyjes <72809149@continental.edu.pe>
2024-03-04 16:40:59 -0500

    welcome page user

--- client/src/pages/user/UserMain.jsx
+++ client/src/pages/user/UserMain.jsx
@@ -1,9 +1,15 @@
+import { Heading } from "@/components/heading";
 import LayoutUser from "./LayoutUser";
+import { Separator } from "@/components/ui/separator";
 
 const UserMain = () => {
     return(
         <LayoutUser>
-            <h1>Welcome User!</h1>
+            <div className="space-y-4 p-8 pt-6">
+                <Heading title="Dashboard User" description="Overview your status" />
+                <Separator />
+                {/* STATUS OF JOB APPLIED BY */}
+            </div>
         </LayoutUser>
     )
 }
