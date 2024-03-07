dce8879c24807cc4f8b6894f867da3fe71d6f8bb
antonyjes <72809149@continental.edu.pe>
2024-03-06 22:09:00 -0500

    job redirect

--- client/src/pages/recruiter/jobs/components/cell-action.jsx
+++ client/src/pages/recruiter/jobs/components/cell-action.jsx
@@ -2,9 +2,11 @@ import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Edit, MoreHorizontal, Trash } from "lucide-react";
 import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom";
 
 export const CellAction = ({data}) => {
     const token = useSelector((state) => state.token);
+    const navigate = useNavigate();
 
     return(
         <>
@@ -17,7 +19,7 @@ export const CellAction = ({data}) => {
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end">
                     <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                    <DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => navigate(`/recruiter/jobs/${data._id}`)}>
                         <Edit className="mr-2 h-4 w-4" />
                         Update
                     </DropdownMenuItem>
--- client/src/pages/recruiter/jobs/components/client.jsx
+++ client/src/pages/recruiter/jobs/components/client.jsx
@@ -4,13 +4,16 @@ import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { Plus } from "lucide-react"
 import { columns } from "./columns"
+import { useNavigate } from "react-router-dom"
 
 export const JobClient = ({ data }) => {
+    const navigate = useNavigate();
+
     return(
         <>
             <div className="flex items-center justify-between">
                 <Heading title="Jobs" description="Manage your jobs" />
-                <Button>
+                <Button onClick={() => navigate("/recruiter/jobs/new")}>
                     <Plus className="mr-2 h-4 w-4" />
                     Add New
                 </Button>
