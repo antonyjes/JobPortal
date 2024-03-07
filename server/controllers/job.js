a5e2925a373686d41505153d633045d5d0fa0e3c
antonyjes <72809149@continental.edu.pe>
2024-03-06 23:45:42 -0500

    createJob api

--- server/controllers/job.js
+++ server/controllers/job.js
@@ -1,10 +1,35 @@
 import Job from "../models/Job.js"
+import Recruiter from "../models/Recruiter.js"
 
 //READ
 export const getJobs = async (req, res) => {
     try {
         const jobs = await Job.find();
         res.status(200).json(jobs);
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
+}
+
+//CREATE
+export const createJob = async (req, res) => {
+    try {
+        const {title, description, location, category, requirements, jobType, status, salary, recruiterId} = req.body;
+        const recruiter = await Recruiter.findById(recruiterId);
+        const newJob = new Job({
+            title,
+            description,
+            location,
+            category,
+            requirements,
+            jobType,
+            status,
+            salary,
+            recruiterId,
+            recruiterName: `${recruiter.firstName} ${recruiter.lastName}`,
+        });
+        const savedJob = await newJob.save();
+        res.status(200).json(savedJob);
     } catch (error) {
         res.status(409).json({ message: error.message })
     }
