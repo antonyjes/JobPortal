8a468c11b544917d81207a7d3d365cf76b85060d
antonyjes <72809149@continental.edu.pe>
2024-03-06 23:45:30 -0500

    post route to create job

--- server/routes/job.js
+++ server/routes/job.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
-import { getJobs } from "../controllers/job.js";
+import { createJob, getJobs } from "../controllers/job.js";
 
 const router = express.Router();
 
 //READ
 router.get("/", verifyToken, getJobs);
+
+//CREATE
+router.post("/create", verifyToken, createJob);
 
 export default router;
