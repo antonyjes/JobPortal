afbd29111dd4f908150f37d20b321682d6f999d7
antonyjes <72809149@continental.edu.pe>
2024-03-18 20:30:30 -0500

    public job page

--- client/src/pages/jobHome/HomeJob.jsx
+++ client/src/pages/jobHome/HomeJob.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
 import LayoutHome from "../LayoutHome";
+import { useParams } from "react-router-dom";
+import { JobDetails } from "@/components/job-details";
 
 const HomeJob = () => {
+    const [jobData, setJobData] = useState([]);
+    const { jobId } = useParams()
+
+    const getPublicJob = async () => {
+        const response = await fetch(`http://localhost:3003/jobs/${jobId}/public`, {
+            method: "GET",
+        });
+
+        const data = await response.json();
+        setJobData(data);
+    };
+
+    useEffect(() => {
+        getPublicJob()
+    }, [])
+
+    if (!jobData) return null;
+
     return(
         <LayoutHome>
-            
+            <JobDetails jobData={jobData} />
         </LayoutHome>
     )
 }
