6d857437390fdc47c389e5d6df4aed1af5ff4922
antonyjes <72809149@continental.edu.pe>
2024-03-19 18:26:21 -0500

    create application job API

--- server/controllers/application.js
+++ server/controllers/application.js
@@ -1,11 +1,32 @@
 import Application from "../models/Application.js";
+import User from "../models/User.js";
 
-// READ
+//READ
 export const getUserApplications = async (req, res) => {
     try {
         const { userId } = req.params;
         const applications = await Application.find({ userId: userId });
         res.status(201).json(applications);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
+
+//CREATE
+export const createApplication = async (req, res) => {
+    try {
+        const {jobId, userId} = req.body;
+        const user = await User.findById(userId);
+        const newApplication = new Application({
+            jobId,
+            userId,
+            userName: `${user.firstName} ${user.lastName}`,
+            userCvPath: user.cvPath,
+            notes: "",
+            status: "",
+        });
+        const savedApplication = await newApplication.save();
+        res.status(200).json(savedApplication);
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
--- server/routes/application.js
+++ server/routes/application.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
-import { getUserApplications } from "../controllers/application.js";
+import { createApplication, getUserApplications } from "../controllers/application.js";
 
 const router = express.Router();
 
 //READ
 router.get("/:userId", verifyToken, getUserApplications);
+
+//CREATE
+router.post("/create", verifyToken, createApplication);
 
 export default router;
