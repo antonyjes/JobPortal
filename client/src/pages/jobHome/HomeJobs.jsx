077d80f0cecd6486547f1a86b5e6c9ba01936913
antonyjes <72809149@continental.edu.pe>
2024-03-18 20:30:13 -0500

    formated page and button for a new job section

--- client/src/pages/jobHome/HomeJobs.jsx
+++ client/src/pages/jobHome/HomeJobs.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import LayoutHome from "../LayoutHome";
 import ReactPaginate from "react-paginate";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 const HomeJobs = () => {
   const [jobs, setJobs] = useState([]);
-  const [currentPage, setCurrentPage] = useState(0);  
+  const [currentPage, setCurrentPage] = useState(0);
+  const navigate = useNavigate();
 
   const getPublicJobs = async () => {
     const response = await fetch("http://localhost:3003/jobs/public", {
@@ -20,8 +23,8 @@ const HomeJobs = () => {
   const pageCount = Math.ceil(jobs.length / PER_PAGE);
 
   const handlePageClick = ({ selected: selectedPage }) => {
-    setCurrentPage(selectedPage)
-  }
+    setCurrentPage(selectedPage);
+  };
 
   useEffect(() => {
     getPublicJobs();
@@ -29,7 +32,7 @@ const HomeJobs = () => {
 
   return (
     <LayoutHome>
-      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+      <section className="space-y-6 pb-4 pt-3 md:pb-6 md:pt-5 lg:py-16">
         <div className="container flex max-w-[64rem] gap-4">
           {jobs &&
             jobs.slice(offset, offset + PER_PAGE).map((job) => (
@@ -37,13 +40,13 @@ const HomeJobs = () => {
                 className="max-w-sm rounded overflow-hidden shadow-lg"
                 key={job._id}
               >
-                <div className="px-6 py-4">
+                <div className="px-6 py-2">
                   <div className="font-bold text-xl mb-2">{job.title}</div>
                   <h3>{job.location}</h3>
                   <h3>{job.category}</h3>
                   <h3>{job.jobType}</h3>
                 </div>
-                <div className="px-6 pt-4 pb-2">
+                <div className="px-6 pt-2 pb-2">
                   {job.requirements.length >= 1 &&
                     job.requirements.map((requirement) => (
                       <span
@@ -54,22 +57,30 @@ const HomeJobs = () => {
                       </span>
                     ))}
                 </div>
+                <div className="flex w-full justify-end m-0">
+                  <Button
+                    className="mb-2 mr-2"
+                    onClick={() => navigate(`/jobs/${job._id}`)}
+                  >
+                    Details
+                  </Button>
+                </div>
               </div>
-            ))}            
+            ))}
         </div>
         <div className="flex justify-center mt-4">
-              <ReactPaginate
-                previousLabel={"← Previous"}
-                nextLabel={"Next →"}
-                pageCount={pageCount}
-                onPageChange={handlePageClick}
-                containerClassName={"pagination"}
-                previousLinkClassName={"previous_page"}
-                nextLinkClassName={"next_page"}
-                disabledClassName={"pagination__link--disabled"}
-                activeClassName={"pagination__link--active"}
-              />
-            </div>
+          <ReactPaginate
+            previousLabel={"← Previous"}
+            nextLabel={"Next →"}
+            pageCount={pageCount}
+            onPageChange={handlePageClick}
+            containerClassName={"pagination"}
+            previousLinkClassName={"previous_page"}
+            nextLinkClassName={"next_page"}
+            disabledClassName={"pagination__link--disabled"}
+            activeClassName={"pagination__link--active"}
+          />
+        </div>
       </section>
     </LayoutHome>
   );
