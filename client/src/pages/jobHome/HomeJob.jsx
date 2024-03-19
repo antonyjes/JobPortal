0c6a13f1da3d408f736669c82b508b3f7d5583d1
antonyjes <72809149@continental.edu.pe>
2024-03-18 21:48:57 -0500

    redirect for public job apply

--- client/src/pages/jobHome/HomeJob.jsx
+++ client/src/pages/jobHome/HomeJob.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import LayoutHome from "../LayoutHome";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { JobDetails } from "@/components/job-details";
+import { Button } from "@/components/ui/button";
 
 const HomeJob = () => {
     const [jobData, setJobData] = useState([]);
     const { jobId } = useParams()
+    const navigate = useNavigate()
 
     const getPublicJob = async () => {
         const response = await fetch(`http://localhost:3003/jobs/${jobId}/public`, {
@@ -25,6 +27,9 @@ const HomeJob = () => {
     return(
         <LayoutHome>
             <JobDetails jobData={jobData} />
+            <div className="flex justify-center mt-4">
+                <Button onClick={() => navigate("/login")} className="w-[10rem]">Apply Job</Button>
+            </div>
         </LayoutHome>
     )
 }
