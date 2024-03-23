3377395a11c1b77d8ba0686cd2ebeb858d26fc98
antonyjes <72809149@continental.edu.pe>
2024-03-22 19:47:10 -0500

    datatable for applications

--- client/src/pages/recruiter/applications/components/cell-action.jsx
+++ client/src/pages/recruiter/applications/components/cell-action.jsx
@@ -3,13 +3,18 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel,
 import { setApplications } from "@/state";
 import { Download, Edit, MoreHorizontal } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux"
+import ModalApplication from "../ModalApplication";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 export const CellAction = ({data}) => {
     const dispatch = useDispatch();
     const token = useSelector((state) => state.token);
+    const [showModal, setShowModal] = useState(false);
+    const { jobId } = useParams();
 
     const getApplications = async () => {
-        const response = await fetch(`http://localhost:3003/applications/${jobId}`, {
+        const response = await fetch(`http://localhost:3003/applications/jobs/${jobId}`, {
             method: "GET",
             headers: {Authorization: `Bearer ${token}`}
         });
@@ -18,8 +23,19 @@ export const CellAction = ({data}) => {
         dispatch(setApplications({ applications: data }));
     };
 
+    const downloadCV = (userCvPath) => {
+        const cvFullPath = `http://localhost:3003/assets/cvs/${userCvPath}`;
+        window.open(cvFullPath, "_blank"); 
+    }
+
     return(
-        <>
+        <>  
+            <ModalApplication
+                showModal={showModal}
+                setShowModal={setShowModal}
+                currentApplication={data}
+                getApplications={getApplications}
+            />
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <Button variant="ghost" className="h-8 w-8 p-0">
@@ -29,11 +45,11 @@ export const CellAction = ({data}) => {
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end">
                     <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                    <DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setShowModal(true)}>
                         <Edit className="mr-2 h-4 w-4" />
                         Update status
                     </DropdownMenuItem>
-                    <DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => downloadCV(data?.userCvPath)}>
                         <Download className="mr-2 h-4 w-4" />
                         Download CV
                     </DropdownMenuItem>
--- client/src/pages/recruiter/applications/components/client.jsx
+++ client/src/pages/recruiter/applications/components/client.jsx
@@ -3,11 +3,11 @@ import { Heading } from "@/components/heading"
 import { Separator } from "@/components/ui/separator"
 import { columns } from "./columns"
 
-export const ApplicationClient = ({ data }) => {
+export const ApplicationClient = ({ data, title }) => {
     return(
         <>
             <div className="flex items-center justify-between">
-                <Heading title={`Applications for ${data?.jobTitle}`} description="Manage your applicants for this job" />
+                <Heading title={`Applications for ${title}`} description="Manage your applicants for this job" />
             </div>
             <Separator />
             <DataTable columns={columns} data={data} searchKey="userName" />
