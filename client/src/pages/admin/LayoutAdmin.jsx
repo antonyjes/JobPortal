816f62d0d231473505243f404a93e4c1bd43b125
antonyjes <72809149@continental.edu.pe>
2024-03-25 22:23:51 -0500

    fix route for applications and add one for admin profile

--- client/src/pages/admin/LayoutAdmin.jsx
+++ client/src/pages/admin/LayoutAdmin.jsx
@@ -4,7 +4,8 @@ const LayoutAdmin = ({children}) => {
     const items = [
         {title: "Dashboard", href: "/admin/home"},
         {title: "Recruiters", href: "/admin/recruiters"},
-        {title: "Applicants", href: "/admin/applicants"}
+        {title: "Applications", href: "/admin/applications"},
+        {title: "Profile", href: "/admin/profile"},
     ]
 
     return(
