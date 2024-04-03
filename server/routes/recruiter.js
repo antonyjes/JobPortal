6ce5b7a12a843b5b714075c6d834e9bfc5081e34
antonyjes <72809149@continental.edu.pe>
2024-04-03 18:20:12 -0500

    countRecruiters route

--- server/routes/recruiter.js
+++ server/routes/recruiter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth.js";
-import { deleteRecruiter, getRecruiters } from "../controllers/recruiter.js";
+import { countRecruiters, deleteRecruiter, getRecruiters } from "../controllers/recruiter.js";
 
 const router = express.Router();
 
 //READ
 router.get("/", verifyToken, getRecruiters);
+router.get("/count/all", verifyToken, countRecruiters);
 
 //DELETE
 router.delete("/:recruiterId/delete", verifyToken, deleteRecruiter);
