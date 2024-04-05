e0e59e5d0e62af042760cb0dbb565179d99e4b4d
antonyjes <72809149@continental.edu.pe>
2024-04-05 12:30:58 -0500

    main recruiter dashboard

--- client/src/pages/recruiter/RecruiterMain.jsx
+++ client/src/pages/recruiter/RecruiterMain.jsx
@@ -3,8 +3,39 @@ import LayoutRecruiter from "./LayoutRecruiter";
 import { Separator } from "@/components/ui/separator";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { UserRound, Building2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const RecruiterMain = () => {
+    const token = useSelector((state) => state.token);
+    const [totalApplications, setTotalApplications] = useState(0);
+    const [totalJobs, setTotalJobs] = useState(0);
+    
+    const countJobs = async () => {
+        const response = await fetch("http://localhost:3003/jobs/count/all", {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}` },
+        });
+
+        const data = await response.json();
+        setTotalJobs(data);
+    }
+
+    const countApplications = async () => {
+        const response = await fetch("http://localhost:3003/applications/count/all", {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}` },
+        });
+
+        const data = await response.json();
+        setTotalApplications(data);
+    }
+
+    useEffect(() => {
+        countApplications();
+        countJobs();
+    }, []);
+
     return(
         <LayoutRecruiter>
             <div className="space-y-4 p-8 pt-6">
@@ -14,13 +45,13 @@ const RecruiterMain = () => {
                     <Card>
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
                             <CardTitle className="text-sm font-medium">
-                                Applicants
+                                Applications
                             </CardTitle>
                             <UserRound className="h-4 w-4 text-muted-foreground" />
                         </CardHeader>
                         <CardContent>
                             <div className="text-2xl font-bold">
-                                {120}
+                                {totalApplications}
                             </div>
                         </CardContent>
                     </Card>
@@ -33,7 +64,7 @@ const RecruiterMain = () => {
                         </CardHeader>
                         <CardContent>
                             <div className="text-2xl font-bold">
-                                {5}
+                                {totalJobs}
                             </div>
                         </CardContent>
                     </Card>
