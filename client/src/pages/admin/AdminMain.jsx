85426526e4d9df136a9a698679b5ef05e88fb1b0
antonyjes <72809149@continental.edu.pe>
2024-04-08 20:14:40 -0500

    fetch data for the chart

--- client/src/pages/admin/AdminMain.jsx
+++ client/src/pages/admin/AdminMain.jsx
@@ -5,12 +5,14 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import LayoutAdmin from "./LayoutAdmin"
 import { useSelector } from "react-redux"
 import { useEffect, useState } from "react"
+import { BarChart } from "@/components/bar-chart"
 
 const AdminMain = () => {
     const token = useSelector((state) => state.token);
     const [totalRecruiters, setTotalRecruiters] = useState(0);
     const [totalJobs, setTotalJobs] = useState(0);
     const [totalApplications, setTotalApplications] = useState(0);
+    const [applicationsStatusData, setApplicationsStatusData] = useState({});
 
     const countRecruiters = async () => {
         const response = await fetch("http://localhost:3003/recruiters/count/all", {
@@ -42,10 +44,39 @@ const AdminMain = () => {
         setTotalApplications(data);
     }
 
+    const getApplications = async () => {
+        const response = await fetch("http://localhost:3003/applications", {
+            method: "GET",
+            headers: {Authorization: `Bearer ${token}`}
+        });
+
+        const data = await response.json();
+        const statusData = countStatusData(data);
+        setApplicationsStatusData(statusData);
+    }
+
+    const countStatusData = (data) => {
+        const statusCount = {};
+        data.forEach(item => {
+            const month = new Date(item.createdAt).getMonth();
+            if (!statusCount[month]) {
+                statusCount[month] = {};
+            }
+            if (statusCount[month][item.status]) {
+                statusCount[month][item.status]++;
+            } else {
+                statusCount[month][item.status] = 1;
+            }
+        });
+
+        return statusCount;
+    }
+
     useEffect(() => {
         countRecruiters();
         countJobs();
         countApplications();
+        getApplications();
     }, []);
 
     return(
@@ -96,7 +127,7 @@ const AdminMain = () => {
                 </div>
                 <Separator />
                 <div className="flex justify-center">
-                    {/* BAR CHART FOR JOBS AND STATUS OF APPLICATIONS */}
+                    {Object.keys(applicationsStatusData).length > 0 && <BarChart key={"application"} data={applicationsStatusData} />}
                 </div>
             </div>
         </LayoutAdmin>
