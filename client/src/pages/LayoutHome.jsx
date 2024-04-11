65ae83c24d4c0ef2a961ced54b1be72066a7e7a8
antonyjes <72809149@continental.edu.pe>
2024-04-11 01:08:24 -0500

    dropdown with the image user

--- client/src/pages/LayoutHome.jsx
+++ client/src/pages/LayoutHome.jsx
@@ -1,9 +1,19 @@
 import { HomeFooter } from "@/components/home-footer";
 import { HomeNav } from "@/components/home-nav";
 import { buttonVariants } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+  DropdownMenuItem
+} from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
 import { setLogout } from "@/state";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Edit } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const itemsHome = [
   { title: "Inicio", href: "/" },
@@ -11,7 +21,9 @@ const itemsHome = [
 ];
 
 const LayoutHome = ({ children, isAuth = false, items = itemsHome }) => {
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
 
   return (
     <div className="flex min-h-screen flex-col">
@@ -19,7 +31,28 @@ const LayoutHome = ({ children, isAuth = false, items = itemsHome }) => {
         <div className="flex h-20 items-center justify-between py-6">
           <HomeNav items={items} isAuth={isAuth} />
           {isAuth ? (
-            <nav>
+            <nav className="flex items-center">
+              {user && (
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <img
+                      className="w-[5rem] h-[5rem] p-2 object-cover rounded-full"
+                      src={`http://localhost:3003/assets/${user?.role}s/${user?.picturePath}`}
+                    />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>{user?.firstName} {user?.lastName}</DropdownMenuLabel>
+                    <DropdownMenuLabel>{user?.email}</DropdownMenuLabel>
+                    <DropdownMenuLabel>{user?.role}</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem onClick={() => navigate(`/${user?.role.toLowerCase()}/profile`)}>
+                      <Edit className="mr-2 h-4 w-4" />
+                      Edit profile
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              )}
               <button
                 className={cn(
                   buttonVariants({ variant: "secondary", size: "sm" })
