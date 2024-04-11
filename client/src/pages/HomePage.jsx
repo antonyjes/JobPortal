4a85a7672f73e33083a60e2dddf8bd3b3a89583f
antonyjes <72809149@continental.edu.pe>
2024-04-11 01:08:10 -0500

    change main text to spanish

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -7,13 +7,13 @@ const HomePage = () => {
         <LayoutHome>
             <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
                 <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
-                    <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">A prototype Job Portal App using Shadcn/UI and Express.</h1>
+                    <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">Portal de Empleo de la Municipalidad de Huayucachi</h1>
                     <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-                        I&apos;m building a web app with Express and React and open sourcing everything. Follow along as we figure this out together.
+                        Crea tu perfil y aplica para los trabajos
                     </p>
                     <div className="space-x-4">
                         <a href="/register" className={cn(buttonVariants({size: "lg"}))}>
-                            Get Started
+                            Ver trabajos
                         </a>
                     </div>
                 </div>
