ea4f496083403b9a5be6fa9f6d285093eb6dbf50
antonyjes <72809149@continental.edu.pe>
2024-04-20 20:02:47 -0500

    footer description to spanish

--- client/src/components/home-footer.jsx
+++ client/src/components/home-footer.jsx
@@ -7,17 +7,17 @@ export const HomeFooter = () => {
                 <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
                     <Command />
                     <p className="text-center text-sm leading-loose md:text-left">
-                        Built by{" "}
+                        Construido con{" "}
                         <a href="https://ui.shadcn.com/" target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">shadcn</a>
-                        . Hosted on{" "}
+                        . Subido en{" "}
                         <a href="https://vercel.com" target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">
                             Vercel
                         </a>
-                        . Illustrations by{" "}
+                        . Ilustraciones por{" "}
                         <a href="https://popsy.co" target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">
                             Popsy
                         </a>
-                        . The source code is available on{" "}
+                        . El código fuente se encuentra disponible en{" "}
                         <a href="https://github.com/antonyjes" target="_blank" rel="noreferrer" className="font-medium underline underline-offset-4">
                             Github
                         </a>
